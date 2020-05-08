2a9567af31b7b5a7731ed264bde68787459f1372
Miska Mäkelä <miska.makela@valagroup.com>
2020-05-08 14:47:45 +0300

    4.21 completed

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,26 +1,51 @@
+const jwt = require('jsonwebtoken')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes || 0,
+    user: user._id
   })
 
-  const result = await blog.save()
-  response.status(201).json(result)
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  if (!request.token) {
+    return response.status(401).json({ error: 'token missing' })
+  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const blog = await Blog.findById(request.params.id)
+  if (blog.user.toString() !== decodedToken.id.toString()) {
+    return response.status(403).send({ error: 'cannot delete blog owned by other user' })
+  }
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
---
+++ osa4/blogilista/tests/user_api.test.js
@@ -0,0 +1,104 @@
+const bcrypt = require('bcrypt')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const User = require('../models/user')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+afterAll(() => {
+  mongoose.connection.close()
+})
+
+describe('when there is initially one user at db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  test('list all users', async () => {
+    const result = await api
+      .get('/api/users')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body).toHaveLength(1)
+    // console.log(result.body)
+  })
+})
+
+describe('user creation validation', () => {
+  test('username must be at least 3 chars long', async () => {
+    const newUser = {
+      username: 'mm',
+      name: 'Miska Mäkelä',
+      password: 'passu',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400, '{"error":"User validation failed: username: Path `username` (`mm`) is shorter than the minimum allowed length (3)."}')
+  })
+
+  test('password must be at least 3 chars long', async () => {
+    const newUser = {
+      username: 'miska',
+      name: 'Miska Mäkelä',
+      password: 'pa',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400, '{"error":"password must be at least 3 characters long"}')
+  })
+})
